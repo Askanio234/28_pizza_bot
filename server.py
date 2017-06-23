1c41e5d69b534f8ec7932d7b8b2c9e6a751f2961
Artem Chernyaev <askanio234@gmail.com>
2017-06-23 21:13:17 +0300

    Implement /admin page with use of flask-admin

--- server.py
+++ server.py
@@ -1,13 +1,16 @@
 from flask import Flask, render_template
+from flask_admin import Admin
 from models import Meal, Option, db_session
+from flask_admin.contrib.sqla import ModelView
 
 
 app = Flask(__name__)
 
-@app.route('/admin')
-def load_admin():
-    query = db_session.query(Meal).all()
-    return render_template('admin.html', meals=query)
+
+admin = Admin(app, name='Pizzashop', template_mode='bootstrap3')
+admin.add_view(ModelView(Meal, db_session))
+admin.add_view(ModelView(Option, db_session))
+
 
 if __name__ == '__main__':
     app.run()
